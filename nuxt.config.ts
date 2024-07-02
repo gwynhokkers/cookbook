6657550f2fa1901cabde17d4f732e55bf2516033
gwynhokkers <33204468+gwynhokkers@users.noreply.github.com>
2024-07-02 10:59:23 +0100

    chore(app): initial commit by Nuxt Studio

---
+++ nuxt.config.ts
@@ -0,0 +1,50 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxthq/studio',
+    'nuxt-og-image'
+  ],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
+  },
+  // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
+  routeRules: {
+    '/': { prerender: true }
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  colorMode: {
+    disableTransition: true
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true }
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  future: {
+    compatibilityVersion: 4
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
