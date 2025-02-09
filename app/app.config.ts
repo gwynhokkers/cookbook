5a96fdc6b3c8e147a11d5d0397063204460bfa93
Gwynfor Hockridge <gwyn@inkythesquid.co.uk>
2025-02-09 22:27:01 +0000

    chore: footer links

--- app/app.config.ts
+++ app/app.config.ts
@@ -161,33 +161,43 @@ export default defineAppConfig({
       'icon': 'i-simple-icons-github',
       'to': 'https://github.com/gwynhokkers/cookbook',
       'target': '_blank',
-      'aria-label': 'Docs template on GitHub'
+      'aria-label': 'Cookbook on GitHub'
     }]
   },
   footer: {
     credits: 'Copyright © 2023',
     colorMode: false,
-    links: [{
-      'icon': 'i-simple-icons-nuxtdotjs',
-      'to': 'https://nuxt.com',
-      'target': '_blank',
-      'aria-label': 'Nuxt Website'
-    }, {
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://discord.com/invite/ps2h6QT',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on Discord'
-    }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://x.com/nuxt_js',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
-    }]
+    links: [
+      {
+        'icon': 'i-lucide-globe',
+        'to': 'https://inkythesquid.co.uk',
+        'target': '_blank',
+        'aria-label': 'Inky the squid'
+      },
+      {
+        'icon': 'i-simple-icons-nuxtdotjs',
+        'to': 'https://nuxt.com',
+        'target': '_blank',
+        'aria-label': 'Nuxt Website'
+      },
+      // {
+      //   'icon': 'i-simple-icons-discord',
+      //   'to': 'https://discord.com/invite/ps2h6QT',
+      //   'target': '_blank',
+      //   'aria-label': 'Nuxt UI on Discord'
+      // },
+      // {
+      //   'icon': 'i-simple-icons-x',
+      //   'to': 'https://x.com/nuxt_js',
+      //   'target': '_blank',
+      //   'aria-label': 'Nuxt on X'
+      // },
+      {
+        'icon': 'i-simple-icons-github',
+        'to': 'https://github.com/gwynhokkers/cookbook',
+        'target': '_blank',
+        'aria-label': 'Cookbook on GitHub'
+      }]
   },
   toc: {
     title: 'Table of Contents',
